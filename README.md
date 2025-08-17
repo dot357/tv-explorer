f8c6226ac6fb3047376fc8b5adba07c693444f9b
dot357 <emrecaneskimez@outlook.com>
2025-08-17 22:38:30 +0200

    feat(tailwind): tailwind integration and vite config improvements

--- README.md
+++ README.md
@@ -4,5 +4,33 @@
 TODO: Need to add nvmrc - node version 22 crypto.hash fails on node 18
 [add bug report]()
 
+
+TODO: Lets add es lint support
+
+<!-- https://primevue.org/theming/unstyled/ -->
+add architectural reasoning behind unstyled components
+
 ## Prime vue
 
+
+
+
+## Vite config
+add documentation entry for how to extend alias via vite config and make sure to include same details for tsconfig.app.json
+
+```json
+ {
+    "resolve" : {
+    "alias" : {
+       '@': path.resolve(__dirname, 'src'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@views': path.resolve(__dirname, 'src/views'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@store': path.resolve(__dirname, 'src/store'),
+      '@api': path.resolve(__dirname, 'src/api'),
+    }
+  }
+ }
+ ```
--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 import { Button } from 'primevue';
+import TestComponent from '@components/TestComponent.vue';
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  
 
-  <Button label="Hello World" icon="pi pi-plus" />
+ <div>
+  <h1>
+    TV Explorer
+  </h1>
+
+  <section>
+     <Button label="Hello World" icon="pi pi-plus" />
+
+     <TestComponent />
 
   <Button
     label="Search"
@@ -24,19 +24,7 @@ import { Button } from 'primevue';
     pt:label="text-white font-bold text-lg"
     pt:icon="text-white text-xl"
 />
+  </section>
+ </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
